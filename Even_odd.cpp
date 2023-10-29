b128692956064ac52d55afdec3e9df25eded90e4
pratikkad10 <148075940+pratikkad10@users.noreply.github.com>
2023-10-29 15:41:51 +0530

    Create Even_odd.cpp

---
+++ Even_odd.cpp
@@ -0,0 +1,21 @@
+#checking number is even or odd
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Enter the value"<<endl;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"even value"<<endl;
+    }
+    else{
+        cout<<"odd value"<<endl;
+    }
+
+
+    return 0;
+}
